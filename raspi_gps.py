bf0d94d8543175ae12f35ca9246a51a3698a1fc3
Mark Bakonyi <bakonyimark9785@gmail.com>
2023-06-27 20:05:38 +0200

    reneame, delete

--- raspi_gps.py
+++ raspi_gps.py
@@ -15,6 +15,8 @@ import serial
 from datetime import datetime
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+import numpy as np
+import time
 
 import pathlib
 scriptpath = pathlib.Path(__file__).parent.resolve()
@@ -23,12 +25,6 @@ scriptpath = pathlib.Path(__file__).parent.resolve()
 # to check which serial port is connected to GPS module run: ls -la /dev/serial/by-id 
 port = serial.Serial('/dev/ttyGPS', baudrate=38400, timeout=1) # change default:ttyACM0 if neccessary (to the correct port: ttyGPS)
 gps = UbloxGps(port)
-
-# # ha nem lenne jó a kiküldéskor írt megoldás
-# import time # lehet nem kell
-# utcdate = datetime.datetime.utcnow()
-# timestamp = utcdate + datetime.timedelta(hours=2) #ez igy joooo?
-# vagy lehet import pytz-vel is
 
 # Sets the variables of the influxDB (You can generate an API token from the "API Tokens Tab" in the UI)
 #token = "" # lana_token
@@ -48,8 +44,35 @@ def send2influx(msg2send):
         write_api.write(bucket, org, msg2send)
 #     client.close() # lehet fölösleges
 
-def run():
+# contiounusly checks six GPS signal in every second, weather they are close to each other (in 0.02 range) or not 
+def gps_verify():
+  gpslat_probes = [0, 1, 2, 3, 4, 5]
+  gpslon_probes = [0, 1, 2, 3, 4, 5]
+  geo = gps.geo_coords()
+  while True: # ez így loopban megy, nem biztos, hogy a legjobb megoldás?
+    for x in range(6): # contiounusly rotates the last six data, check in every rotation
+        gpslat_probes[x] = round(geo.lat,2)
+        gpslon_probes[x] = round(geo.lon,2)
+        if gpslat_probes[0] in np.arange(gpslat_probes[1]-0.01, gpslat_probes[1]+0.02,0.01) and gpslat_probes[1] in np.arange(gpslat_probes[2]-0.01, gpslat_probes[2]+0.02,0.01) and gpslat_probes[2] in np.arange(gpslat_probes[3]-0.01, gpslat_probes[3]+0.02,0.01) and gpslat_probes[3] in np.arange(gpslat_probes[4]-0.01, gpslat_probes[4]+0.02,0.01) and gpslat_probes[4] in np.arange(gpslat_probes[5]-0.01, gpslat_probes[5]+0.02,0.01):
+            if gpslon_probes[0] in np.arange(gpslon_probes[1]-0.01, gpslon_probes[1]+0.02,0.01) and gpslon_probes[1] in np.arange(gpslon_probes[2]-0.01, gpslon_probes[2]+0.02,0.01) and gpslon_probes[2] in np.arange(gpslon_probes[3]-0.01, gpslon_probes[3]+0.02,0.01) and gpslon_probes[3] in np.arange(gpslon_probes[4]-0.01, gpslon_probes[4]+0.02,0.01) and gpslon_probes[4] in np.arange(gpslon_probes[5]-0.01, gpslon_probes[5]+0.02,0.01):
+              gps_err4 = 0 # GPS position is acceptable
+              gps_err_msg4 = Point("GPS_Precison_Error") \
+                      .tag("sensor", "sparkfun_ublox_NEO-M9N") \
+                      .field("Error_message", gps_err4) \
+                      .time(datetime.utcnow(), WritePrecision.NS)
+              send2influx(gps_err_msg4)
+              return True
+            else:
+              gps_err4 = 1 # valami int vagy string amit a grafana feldogoz hibaüzenetként
+              gps_err_msg4 = Point("GPS_Precison_Error") \
+                      .tag("sensor", "sparkfun_ublox_NEO-M9N") \
+                      .field("Error_message", gps_err4) \
+                      .time(datetime.utcnow(), WritePrecision.NS)
+              send2influx(gps_err_msg4)
+              return False
+    time.sleep(1)
 
+def run():
     try:
         while True:
             try:
@@ -61,7 +84,7 @@ def run():
                 send2influx(gps_err_msg1)
 
                 geo = gps.geo_coords()
-                
+
                 if geo.lon == 0.0 and geo.lat == 0.0:
                     gps_err2 = 1 # GPS pozicio 0, valoszinuleg nincs GPS jel, nezd meg a kek PPS LED vilagit-e
                     gps_err_msg2 = Point("GPS_Position_Error") \
@@ -109,7 +132,7 @@ def run():
                 # send2influx(lana_gps)
 
                 send2influx(gps_coords)
-                send2influx(heading)                                    
+                send2influx(heading)      
                     
             except (ValueError, IOError) as err:
                 gps_err1 = 1 # Communication Error with GPS module
@@ -118,11 +141,13 @@ def run():
                   .field("Error_message", gps_err1) \
                   .time(datetime.utcnow(), WritePrecision.NS)
                 send2influx(gps_err_msg1)
-                
-        time.sleep(0.50) # biztos ami sicher
+
+        time.sleep(0.50) # biztos ami sicher      
 
     finally:
         port.close()
 
 if __name__ == '__main__':
-    run()
+  # if six GPS signals are in 0.02 range it enables to run the 'run' function
+    while gps_verify() == True: # folyamatosan chekkel (remelem)
+        run()
