d49bb5ca38f1d26e32545abb2d30ec9331c35070
Mark Bakonyi <bakonyimark9785@gmail.com>
2023-08-10 00:32:56 +0200

    extend sleep

--- raspi_gps.py
+++ raspi_gps.py
@@ -157,11 +157,11 @@ def run():
                 setNsend("GPS_speed", "Speed", gps_speed) 
 
                 print("Lefutott a try egyszer",datetime.now())
+
+                time.sleep(1) # sec
                     
             except (ValueError, IOError) as err:
                 setNsend("GPS_Comm_Error", "Error_message", 1) # Communication Error with GPS module
-                
-        time.sleep(0.50) # sec
 
     finally:
         port.close()
